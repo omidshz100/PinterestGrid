e824aa21d740380cf420ff92ae74aba3b383df1f
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2023-11-10 10:57:30 +0100

    Initial Commit

---
+++ PinterestGrid/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PinterestGrid
+//
+//  Created by Omid Shojaeian Zanjani on 10/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PinterestGrid/PinterestGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinterestGridApp.swift
+//  PinterestGrid
+//
+//  Created by Omid Shojaeian Zanjani on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PinterestGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
